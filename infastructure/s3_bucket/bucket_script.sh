c70ddbd6e0f651b44d823ef9a4c1138bd495ea5a
Prince Kyeremeh <prince.kyeremeh@amalitech.com>
2025-05-15 14:27:10 +0000

    Feat: Add script to create S3 bucket and necessary folder structure

---
+++ infastructure/s3_bucket/bucket_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Create S3 buckets
+echo "Creating S3 bucket..."
+aws s3 mb s3://ecommerce-lakehouse-123 --region eu-west-1
+
+# Create folders in the S3 bucket
+echo "Creating folders in the S3 bucket..."
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key raw/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key bronze/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key silver/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key gold/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key archive/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key rejected/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key scripts/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key libs/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key rejected/order_items/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key rejected/orders/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key rejected/products/
+aws s3api put-object --bucket ecommerce-lakehouse-123 --key gold/sales_by_department/
+# Set bucket policies
+
+
+echo "S3 bucket and folders created successfully."
